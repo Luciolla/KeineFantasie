e541f8741b7bbeb7ccc8a82437b2ab4ea482e818
Kirill Voronov <70638098+Luciolla@users.noreply.github.com>
2023-03-01 18:36:55 +1000

    simpleAI
    1) add patrol moving
    2) add  Alarm trigger and bot reaction

---
+++ Assets/Scripts/Components/Enums/CreatureTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Fantasie
+{
+    public enum CreatureTypeEnum : byte
+    {
+        None = 0,
+        Creature = 1,
+        Player = 2,
+        Environment = 4,
+    }
+}
---
+++ Assets/Scripts/Components/Enums/EnemyTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Fantasie
+{
+    public enum EnemyTypeEnum : byte
+    {
+        Melee = 0,
+        FlyMelee = 1,
+        Range = 2,
+        FlyRange = 4,
+    }
+}
